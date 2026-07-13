e8cb186a235b7a46291c7593e5cdec53dc5af682
Paulo Vasconcelos <vasconcelospaulorp@gmail.com>
2026-07-13 10:48:16 -0400

    feat: remove deploymentId configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  deploymentId: process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 16),
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
